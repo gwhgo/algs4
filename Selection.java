0917137dc518fd8339c86c4d4e31b4f51b7c9756
Guoweihao <guoweihao_shanke@163.com>
2020-10-22 09:51:07 +0800

    re commit

---
+++ Selection.java
@@ -0,0 +1,49 @@
+public class Selection extends SortExample {
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        // Print the array, on a single line
+        for (int i = 0; i < a.length; i++)
+            StdOut.println(a[i] + " ");
+        StdOut.println();
+    }
+
+
+    public static boolean isSorted(Comparable[] a) {
+        // Test whether the array entries are in order
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        // Sort a[] into increasing order
+        int N = a.length;
+
+        for (int i = 0; i < N; i++) {
+            // Exchange a[i] with smallest entry with a[i+1 ... N].
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+        }
+        assert isSorted(a);
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        Selection.sort(a);
+        show(a);
+    }
+}
